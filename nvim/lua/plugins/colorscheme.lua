76715c432129a71567da672ab570db6d6bd2cd1b
sinhlh2 <sinhlhvn@gmail.com>
2026-05-10 10:01:20 +0700

    chore: add auto-save

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,3 @@
-local function macos_is_dark()
-  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
-  if not handle then
-    return false
-  end
-  local result = handle:read("*a") or ""
-  handle:close()
-  return result:match("Dark") ~= nil
-end
-
-local function apply_system_theme()
-  if macos_is_dark() then
-    vim.o.background = "dark"
-    vim.cmd.colorscheme("catppuccin-mocha")
-  else
-    vim.o.background = "light"
-    vim.cmd.colorscheme("catppuccin-latte")
-  end
-end
-
-vim.api.nvim_create_autocmd("FocusGained", {
-  group = vim.api.nvim_create_augroup("auto_macos_theme", { clear = true }),
-  callback = apply_system_theme,
-})
-
 return {
   {
     "catppuccin/nvim",
@@ -40,7 +15,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = apply_system_theme,
+      colorscheme = "catppuccin",
     },
   },
 }
